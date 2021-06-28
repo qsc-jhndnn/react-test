ce2edf0be7c61b1ffa7079ea8aa92aa905d2c271
John Dunn <John.Dunn@qsc.com>
2021-06-28 14:46:22 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Editor from '@monaco-editor/react';
+
+function handleEditorDidMount(editor, monaco) 
+{
+  alert('hey');
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{width: '100vw', height: '100vh', boxSizing:'border-box', overflow: 'hidden'}}>
+      <Editor
+        language="lua"
+        theme='vs-dark'
+        value="function foo() return 'hey' end"
+        onMount={handleEditorDidMount}
+        />
     </div>
   );
 }
