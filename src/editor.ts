aefcc54ded4b5cf6953eaaf72eac891d8a1946f3
John Dunn <John.Dunn@qsc.com>
2021-06-30 11:24:48 -0600

    parse lua to generate completion items

--- src/editor.ts
+++ src/editor.ts
@@ -1,4 +1,5 @@
 import { Monaco } from "@monaco-editor/react";
+import parserx from "./parser";
 
 export default class editorX {
 
@@ -12,8 +13,8 @@ export default class editorX {
   }
 
   parseLua(lua: any) {
-  //  let parser = new parserx();
-  //  parser.parse(lua);
+    let parser = new parserx();
+    parser.parse(lua);
   }
 
   createMathOptions() {
@@ -87,6 +88,7 @@ export default class editorX {
       {
         label: 'new function',
         kind: this.monaco.languages.CompletionItemKind.Snippet,
+        detail: "function snippet",
         documentation: "create a function",
         /* eslint-disable no-template-curly-in-string */
         insertText: 'function ${1:functionName}(${2:args})\n  ${3:body}\nend\n',
@@ -158,12 +160,31 @@ export default class editorX {
   }
 
   provideCompletionItems(model : any, position : any, context : any, token: any) {
-
     let line = model.getValueInRange({startLineNumber: position.lineNumber, startColumn: 1, endLineNumber: position.lineNumber, endColumn: position.column});
     let tokens = line.split(/(\s+)/);
 
     let tok = tokens[tokens.length-1];
-    return { suggestions: this.getCompletionOptions(tok) };
+    // get suggestions
+    let sugs = this.getCompletionOptions(tok);
+
+    // for right now if the user pressed a '.' don't show the parsed stuff
+
+    if(context.triggerKind !== this.monaco.languages.CompletionTriggerKind.TriggerCharacter) {
+      // parse lua to get variables and functions
+      let content = model.getValueInRange({startLineNumber: 1, startColumn: 1, endLineNumber: position.lineNumber, endColumn: position.column});
+      let parser = new parserx();
+      let vars = parser.parse(content);
+      vars.forEach(element => {
+        sugs.push({
+          label: element.label,
+          detail: element.detail,
+          kind: element.kind,
+          insertText: element.insertText,
+          insertTextRules: element.insertRule
+        })
+      });
+    }
+    return { suggestions: sugs };
   }
 
   resolveCompletionItem(item: any, token: any) {
